61b0cd9c0f9aa1f16f394bd616ca0a3dbd5cfd01
Lavanya <getlavanyat@gmail.com>
2023-05-06 11:04:04 +0530

    user login page

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
+
 # posts=[
 #     {
 #         'author':'CoreMS',
@@ -18,10 +19,11 @@ from .models import Post
 # ]
 
 def home(request):
-    context={
-        'posts':Post.objects.all()
+    context = {
+        'posts': Post.objects.all()
     }
-    return render(request,'blog/Home.html',context)
+    return render(request, 'blog/Home.html', context)
+
 
 def about(request):
-    return render(request,'blog/about.html',{'title':'About'})
+    return render(request, 'blog/about.html', {'title': 'About'})
